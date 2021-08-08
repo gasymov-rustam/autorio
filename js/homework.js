e8a81feda800c95e09ef3d4a7becdbea5111093d
rustam-cloud <78147561+rustam-cloud@users.noreply.github.com>
2021-08-08 20:53:12 +0300

    last changes of autorio project

--- js/homework.js
+++ js/homework.js
@@ -1,7 +1,4 @@
 let CAR = JSON.parse(DATA);
-delete CAR[0].consume;
-delete CAR[0].seller;
-// delete CAR[1].rating;
 
 const list = document.getElementById("list");
 const searchFormSquare = document.getElementById("search-form__square");
@@ -13,7 +10,7 @@ const filterField = ['make', 'transmission', 'color'];
 const carArrLength = CAR.length;
 
 searchFormSquare.addEventListener('click', e => {
-  const currentBtn = e.target.closest('button');
+  const currentBtn = e.target.closets('button');
   if (currentBtn) {
     const type = currentBtn.dataset.type;
     const listCurrentClass = Array.from(list.classList).find(className => className.includes('col'));
@@ -28,22 +25,23 @@ searchFormSquare.addEventListener('click', e => {
     })
   }
 })
-// -----------------------------------------------------------------------------------------------------
 
 searchFormSearch.addEventListener('keyup', e => {
   const searchFields = ['make', 'model', 'year', 'vin', 'country'];
   const query = e.target.value.trim().toLowerCase().split(' ').filter(word => !!word);
+  console.log(query);
   CAR = JSON.parse(DATA).filter(car => {
     return query.every(word => {
       return searchFields.some(field => {
-        return String(car[field]).toLowerCase().includes(word)
+        return String(car[field]).toLowerCase().includes(word);
       })
     })
   })
+
   if (e.code !== 'Enter') {
     if (CAR.length !== carArrLength) {
       searchCurrentResult.classList.remove('search-form_disactive');
-      searchCurrentResult.value = `found ${CAR.length} results`;
+      searchCurrentResult.value = `found ${CAR.length} results`
     } else {
       searchCurrentResult.classList.add('search-form_disactive');
     }
@@ -53,8 +51,9 @@ searchFormSearch.addEventListener('keyup', e => {
 searchFormSearch.addEventListener('submit', e => {
   e.preventDefault();
   searchCurrentResult.classList.add('search-form_disactive');
-  printHtml(list, CAR);
+  printHtml(list, CAR)
 })
+// -----------------------------------------------------------------------------------------------------
 
 printHtml(list, CAR);
 
@@ -63,80 +62,81 @@ function printHtml(section, arr) {
 }
 
 function carsArray(arr) {
-  return arr.map(car => carCard(car))
+  return arr.map(car => carCard(car));
 }
 
 function carCard(card) {
   let str = '';
   for (let i = 0; i < 5; i++) {
     if (card.rating > i + .5) {
-      str += '';
+      str += `<i class="fas fa-star"></i>&#x20`;
     } else if (card.rating === i + .5) {
-      str += '';
+      str += `<i class="fas fa-star-half-alt"></i>&#x20`;
     } else {
-      str += '';
+      str += `<i class="far fa-star"></i>&#x20`;
     }
   }
 
   const CONSUME = `<dl>
-                    <dt class="car-info__consume_road">road: ${card.consume?.road || "not indicated"} </dt>
-                    <dt class="car-info__consume_city">city: ${card.consume?.city || "not indicated"} </dt>
-                    <dt class="car-info__consume_mixed">mixed: ${card.consume?.mixed || "not indicated"}</dt>
-                  </dl>`;
+  <dt class="car-info__consume_road">road: ${card.consume?.road || "not indicated"} </dt>
+  <dt class="car-info__consume_city">city: ${card.consume?.city || "not indicated"} </dt>
+  <dt class="car-info__consume_mixed">mixed: ${card.consume?.mixed || "not indicated"}</dt>
+</dl>`;
 
   return `<div class="card">
-  <img src="${card.img}" alt="${card.make} ${card.model}" class="car-photo">
-  <div class="car-info">
-      <h2 class="car-info__make">"${card.make}"</h2>
-      <h3 class="car-info__model">"${card.model}"</h3>
-      <dl>
+          <img src="${card.img}" alt="${card.make} ${card.model}" class="car-photo">
+          <div class="car-info">
+          <h2 class="car-info__make">"${card.make}"</h2>
+          <h3 class="car-info__model">"${card.model}"</h3>
+          <dl>
           <div class="car-info__country">
-              <dt>country -</dt>
-              <dd>${card.country}</dd>
+          <dt>country -</dt>
+          <dd>${card.country}</dd>
           </div>
           <div class="car-info__color">
-              <dt>color -</dt>
-              <dd>${card.color}</dd>
+          <dt>color -</dt>
+          <dd>${card.color}</dd>
           </div>
           <div class="car-info__year">
-              <dt>year -</dt>
-              <dd>${card.year}</dd>
+          <dt>year -</dt>
+          <dd>${card.year}</dd>
           </div>
           <div class="car-info__rating">
-  <!--                        <dd>${card?.rating}</dd>-->
-              <dd>${str || "not inspected"}</dd>
+          <!--                        <dd>${card?.rating}</dd>-->
+          <dd>${str || "not inspected"}</dd>
           </div>
           <div class="car-info__price">
-              <dt>price -</dt>
-              <dd>${card.price}&#x20$</dd>
+          <dt>price -</dt>
+          <dd>${card.price}&#x20$</dd>
           </div>
           <div class="car-info__engine-volume">
-              <dt>engine volume -</dt>
-              <dd>&#128663; ${card.engine_volume} l.</dd>
+          <dt>engine volume -</dt>
+          <dd>&#128663; ${card.engine_volume} l.</dd>
           </div>
           <div class="car-info__transmission">
-              <dt>transmission -</dt>
-              <dd>&#128663; ${card.transmission}</dd>
+          <dt>transmission -</dt>
+          <dd>&#128663; ${card.transmission}</dd>
           </div>
           <div class="car-info__odo">
-              <dt>odo -</dt>
-              <dd>&#9942; ${card.odo} km</dd>
+          <dt>odo -</dt>
+          <dd>&#9942; ${card.odo} km</dd>
           </div>
           <div class="car-info__fuel">
-              <dt>fuel -</dt>
-              <dd>⛽  ${card.fuel}</dd>
+          <dt>fuel -</dt>
+          <dd>⛽  ${card.fuel}</dd>
           </div>
           <div class="car-info__car-consume">
-              <dt>consume -</dt>
-              <dd>${card.consume ? CONSUME : "not indicated"}</dd>    
+          <dt>consume -</dt>
+          <dd>${card.consume ? CONSUME : "not indicated"}</dd>    
           </div>
           <div class="car-info__seller">
-              <dt>seller -</dt>
-              <dd>${card?.seller || "not indicated"}</dd>    
+          <dt>seller -</dt>
+          <dd>${card?.seller || "not indicated"}</dd>    
           </div>
-      </dl>
-  </div>
-  </div>`;
+          </dl>
+          </div>
+        </div>`;
+
 }
 
 function sortTo(arr, smartKey, order) {
@@ -153,142 +153,69 @@ searchFormSort.addEventListener('change', e => {
   const currentInput = e.target.value.trim().toLowerCase().split('/');
   const [key, order] = currentInput;
   CAR.sort(sortTo(CAR, key, order));
-  printHtml(list, CAR);
-})
-// -------------------------------------------------------------
-
-/* renderFilterForm(filterFormEl, CAR)
-
-filterFormEl.addEventListener('submit', e => {
-  e.preventDefault();
-  const query = filterField.map(field => {
-    const values = Array.from(filterFormEl[field]).map(input => input.checked && input.value).filter(word => !!word);
-    return values;
-  })
-
-  CAR = JSON.parse(DATA).filter(car => {
-    return query.every(values => {
-      return filterField.some(field => {
-        return values.length > 0 ? values.includes(String(car[field])) : true;
-      })
-    })
-  })
-
   printHtml(list, CAR)
-}) */
-
-
-/* filterFormEl.addEventListener('submit', e => {
-  e.preventDefault()
-  const query = filterFields.map(field => {
-    const values = Array.from(filterFormEl[field]).map(input => input.checked && input.value).filter(value => !!value)
-    return values
-  })
-  console.log(query);
-
-  // cars = JSON.parse(DATA).filter(car => {
-  //   return query.every(values => {
-  //     return filterFields.some(field => {
-  //       return values.length > 0 ? values.includes(String(car[field])) : true
-  //     })
-  //   })
-  // })
-  cars = JSON.parse(DATA).filter(car => {
-    return query.every((values, idx) => {
-      return values.length > 0 ? values.includes(String(car[filterFields[idx]])) : true
-    })
-  })
-  renderCardList(listEl, cars)
 })
- */
 
-/* function renderFilterForm(elemForRender, dataArray) {
-  const filtersHTML = `<div class="fieldsets-wrap">
-                            ${createFilterFieldsets(dataArray).join('')}
-                      </div>`
-  elemForRender.insertAdjacentHTML('afterbegin', filtersHTML)
-}
- */
-
-/* function createFilterFieldsets(carsArray) {
-  return filterField.map(filed => {
-    const values = Array.from(new Set(carsArray.map(car => car[field])));
-    return createFilterFieldsets(field, values)
-  })
-} */
-
-/* function createFilterFielset(field, values) {
-  const fieldHTML = values.map(value => createFilterField(field, value)).join('');
-  return `<fieldset>
-            <legend>${field}</legend>
-            ${fieldHTML}
-          </fieldset>`
-}
- */
-
-/* function createFilterField(field, value) {
-  return `<label>
-            <input type="checkbox" name="${field}" value="${value}">
-            <span>${value}</span>
-          </label>`
-} */
+renderFilterForm(filterFormEl, CAR)
 
 function renderFilterForm(elemForRender, dataArray) {
   const filtersHTML = `<div class="fieldsets-wrap">
                         ${createFilterFieldset(dataArray).join('')}
-                      </div>`;
-  elemForRender.insertAdjacentHTML('afterbegin', filtersHTML);
+                      </div>`
+  elemForRender.insertAdjacentHTML('afterbegin', filtersHTML)
 }
 
-function createFilterFieldset(dataArray){
+function createFilterFieldset(dataArray) {
   return filterField.map(field => {
     const values = Array.from(new Set(dataArray.map(car => car[field])));
     return createFilterFielset(field, values);
   })
 }
 
-function createFilterFielset(field, values){
-  const fieldHTML = values.map(value => createFilterField(field, value)).join('');
+function createFilterFielset(field, values) {
+  const fieldsHTML = values.map(value => createFilterField(field, value)).join('');
   return `<fieldset>
             <legend>${field}</legend>
-            ${fieldHTML}
+            ${fieldsHTML}
           </fieldset>`
 }
 
-function createFilterField(field, value){
-  return `<label for="${value}">
-            <input type="checkbox" name="${field}" value="${value}" id="${value}">
+function createFilterField(field, value) {
+  return `<label>
+            <input type="checkbox" name="${field}" value="${value}">
             <span>${value}</span>
           </label>`
 }
 
-renderFilterForm(filterFormEl,CAR);
-
-filterFormEl.addEventListener('submit', e => {
+/* filterFormEl.addEventListener('submit', e => {
   e.preventDefault();
-  /* const query = filterField.map(field => {
-    const values = Array.from(filterFormEl[field]).map(input => input.checked && input.value).filter(word => !!word);
-    return values;
-  }) */
   const query = filterField.map(field => {
-    // const values = Array.from(filterFormEl[field]).reduce((total, input) >= (input.checked ? total.push(input.value): true),[])
-    // const values = Array.from(filterFormEl[field]).reduce((total, input) >= (input.checked && total.push(input.value)),[]);
-    const values = Array.from(filterFormEl[field]).reduce((total, input) => {
-      if (input.checked) total.push(input.value);
-      return total;
-    }, []);
+    const values = Array.from(filterFormEl[field]).map(input => input.checked && input.value).filter(word => !!word);
     return values;
   })
 
-  /* CAR = JSON.parse(DATA).filter(car =>{
+  CAR = JSON.parse(DATA).filter(car=> {
     return query.every(values => {
       return filterField.some(field => {
-        return values.length > 0 ? (values.includes(String(car[field]))) : true;
+        return values.length > 0 ? values.includes(String(car[field])):true
       })
     })
-  }) */
+  })
 
-  CAR = JSON.parse(DATA).filter(car=>{
+  printHtml(list, CAR);
+}) */
+
+filterFormEl.addEventListener('submit', e => {
+  e.preventDefault();
+  const query = filterField.map(field => {
+    const values = Array.from(filterFormEl[field]).reduce((total, input) => {
+      if (input.checked) total.push(input.value);
+      return total;
+    }, [])
+    return values;
+  })
+  console.log(query);
+  CAR = JSON.parse(DATA).filter(car => {
     return query.every((values, idx) => {
       return values.length > 0 ? values.includes(String(car[filterField[idx]])) : true;
     })
